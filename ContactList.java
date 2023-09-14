6ad2e3f2583ba4dc6e8f02620fa25d9165aad168
Alex Charlot <acharlot2002@gmail.com>
2023-09-14 18:34:47 -0400

    select is functional

--- ContactList.java
+++ ContactList.java
@@ -2,18 +2,16 @@ import java.io.*;
 import java.util.*;
 
 //example javadoc comment
-    /**
-     * Constructs a set with the elements in list
-     * 
-     * @param list collection of elements to be added to set
-     * @returns result 
-     */
+/**
+ * Constructs a set with the elements in list
+ * 
+ * @param list collection of elements to be added to set
+ * @returns result
+ */
 
+public class ContactList {
 
-
-public class ContactList{
-
-    private Table table; //table that includes linked list of contacts
+    private Table table; // table that includes linked list of contacts
 
     // private static ContactList cl1;
     // private static ContactList cl2;
@@ -22,174 +20,168 @@ public class ContactList{
      * 
      * @param args
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter filename for contact list 1> ");
-        String filename1 = sc.nextLine(); //name of first file
-        
+        String filename1 = sc.nextLine(); // name of first file
+
         System.out.print("Enter filename for contact list 2> ");
-        String filename2 = sc.nextLine(); //name of second file
+        String filename2 = sc.nextLine(); // name of second file
 
-        ContactList cl1 = new ContactList(); //used to create first database
-        ContactList cl2 = new ContactList(); //udsed to create second database
+        ContactList cl1 = new ContactList(); // used to create first database
+        ContactList cl2 = new ContactList(); // udsed to create second database
 
-
-        //only keep going if database is successfullly created
-        if(cl1.createDatabase(filename1) && cl2.createDatabase(filename2)){ 
+        // only keep going if database is successfullly created
+        if (cl1.createDatabase(filename1) && cl2.createDatabase(filename2)) {
             System.out.println(cl1.table.toString());
             System.out.println(cl2.table.toString());
-        
+
         }
 
-        //display menu syntax (not in the right place yet)
+        // display menu syntax (not in the right place yet)
         System.out.println("Welcome to database display\n");
-        
-        //starts the display menu loop
+
+        // starts the display menu loop
         displayMenu(cl1, cl2, sc);
-        
-        //close scanner
+
+        // close scanner
         sc.close();
 
     }
 
-    public boolean createDatabase(String filename){
+    public boolean createDatabase(String filename) {
         boolean result = true;
-        try{
-            //scanner of the file
+        try {
+            // scanner of the file
             Scanner filesc = new Scanner(new File(filename));
             String contact_type = filesc.nextLine();
 
-            if(contact_type.equals("P")){
+            if (contact_type.equals("P")) {
                 System.out.println("personal contact");
                 table = new Table("Personal Contacts");
 
-                //first contact is the head of the list
+                // first contact is the head of the list
                 Table.Node current = createPersonalContact(filesc.nextLine());
-                
+
                 table.setHead(current);
-                
+
                 Table.Node next;
 
-                while(filesc.hasNextLine()){ //iterate through remaining contacts
+                while (filesc.hasNextLine()) { // iterate through remaining contacts
                     next = createPersonalContact(filesc.nextLine());
                     current.setNext(next);
                     current = next;
                 }
 
-                //set tail
+                // set tail
                 table.setTail(current);
 
-            }
-            else if(contact_type.equals("W")){
+            } else if (contact_type.equals("W")) {
                 System.out.println("work contact");
 
                 table = new Table("Work Contacts");
 
-                //first contact is the head of the list
+                // first contact is the head of the list
                 Table.Node current = createWorkContact(filesc.nextLine());
-                
+
                 table.setHead(current);
-                
+
                 Table.Node next;
 
-                while(filesc.hasNextLine()){ //iterate through remaining contacts
+                while (filesc.hasNextLine()) { // iterate through remaining contacts
                     next = createWorkContact(filesc.nextLine());
                     current.setNext(next);
                     current = next;
                 }
 
-                //set tail
+                // set tail
                 table.setTail(current);
-            }
-            else{
+            } else {
                 System.out.println("Problem, not personal or work contact");
             }
 
-            //close the scanner
+            // close the scanner
             filesc.close();
 
-        } catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println("Incorrect file name.");
             result = false;
         }
         return result;
     }
 
-    public Table.Node createPersonalContact(String info){
+    public Table.Node createPersonalContact(String info) {
         System.out.println("INFO_> " + info);
         String[] infoArray = info.split(",");
 
-        //remove the whitespace from each element in the array, 
-        //and trim whitespace from address and city
-        for(int i = 0; i < infoArray.length; i++){
-            if(i == 4 || i == 5){
+        // remove the whitespace from each element in the array,
+        // and trim whitespace from address and city
+        for (int i = 0; i < infoArray.length; i++) {
+            if (i == 4 || i == 5) {
                 infoArray[i] = infoArray[i].trim();
-            }
-            else{
-                infoArray[i] = infoArray[i].replaceAll("\\s","");
+            } else {
+                infoArray[i] = infoArray[i].replaceAll("\\s", "");
             }
         }
-        PersonalContact pc = new PersonalContact(infoArray[0], infoArray[1], infoArray[2], 
-                                 infoArray[3], infoArray[4], infoArray[5],
-                                 infoArray[6], Integer.parseInt(infoArray[7]), infoArray[8]);
-        
+        PersonalContact pc = new PersonalContact(infoArray[0], infoArray[1], infoArray[2],
+                infoArray[3], infoArray[4], infoArray[5],
+                infoArray[6], Integer.parseInt(infoArray[7]), infoArray[8]);
+
         Table.Node node = table.new Node(pc);
-        
+
         return node;
     }
 
-    public Table.Node createWorkContact(String info){
+    public Table.Node createWorkContact(String info) {
         System.out.println("INFO_> " + info);
         String[] infoArray = info.split(",");
 
-        //remove the whitespace from each element in the array, 
-        //and trim whitespace from address and city, title, company, and department
-        for(int i = 0; i < infoArray.length; i++){
-            if(i == 5 || i == 6 || i == 9 || i == 10 || i == 11){
+        // remove the whitespace from each element in the array,
+        // and trim whitespace from address and city, title, company, and department
+        for (int i = 0; i < infoArray.length; i++) {
+            if (i == 5 || i == 6 || i == 9 || i == 10 || i == 11) {
                 infoArray[i] = infoArray[i].trim();
-            }
-            else{
-                infoArray[i] = infoArray[i].replaceAll("\\s","");
+            } else {
+                infoArray[i] = infoArray[i].replaceAll("\\s", "");
             }
         }
-// infoArray[0] last
-// infoArray[1] first
-// infoArray[2] status
-// infoArray[3] email
-// infoArray[4] phone
-// infoArray[5] street address
-// infoArray[6] city
-// infoArray[7] state
-// infoArray[8] zip
-// infoArray[9] title
-// infoArray[10] company
-// infoArray[11] department
+        // infoArray[0] last
+        // infoArray[1] first
+        // infoArray[2] status
+        // infoArray[3] email
+        // infoArray[4] phone
+        // infoArray[5] street address
+        // infoArray[6] city
+        // infoArray[7] state
+        // infoArray[8] zip
+        // infoArray[9] title
+        // infoArray[10] company
+        // infoArray[11] department
 
+        WorkContact wc = new WorkContact(infoArray[0], infoArray[1], infoArray[2],
+                infoArray[5], infoArray[6], infoArray[7],
+                Integer.parseInt(infoArray[8]), infoArray[4], infoArray[3], infoArray[9], infoArray[10], infoArray[11]);
 
-        WorkContact wc = new WorkContact(infoArray[0], infoArray[1], infoArray[2], 
-                                 infoArray[5], infoArray[6], infoArray[7],
-                                 Integer.parseInt(infoArray[8]), infoArray[4], infoArray[3], infoArray[9], infoArray[10], infoArray[11]);
-        
         Table.Node node = table.new Node(wc);
-        
+
         return node;
     }
 
-    public static void displayMenu(ContactList cl1, ContactList cl2, Scanner sc){
-        //initialize input
+    public static void displayMenu(ContactList cl1, ContactList cl2, Scanner sc) {
+        // initialize input
         int input = 1;
-        //while loop to keep displaying menu until user quits
-        while (input != 0){
-            //print menu
+        // while loop to keep displaying menu until user quits
+        while (input != 0) {
+            // print menu
             System.out.println("Please make a choice:");
             String choice = "\t0) Quit\n\t1) Intersect\n\t2) Difference\n\t3) Union\n\t4) Select" +
-                "\n\t5) Remove\n\t6) Print both tables";
+                    "\n\t5) Remove\n\t6) Print both tables";
             System.out.println(choice);
             System.out.print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ");
             input = sc.nextInt();
-            //switch statement to determine what to do based on input
-            switch(input){
+            // switch statement to determine what to do based on input
+            switch (input) {
                 case 0:
                     System.out.println("Goodbye!");
                     break;
@@ -197,16 +189,16 @@ public class ContactList{
                     intersect(cl1, cl2);
                     break;
                 case 2:
-                    System.out.println("Difference"); //TODO
+                    System.out.println("Difference"); // TODO
                     break;
                 case 3:
-                    System.out.println("Union"); //TODO
+                    System.out.println("Union"); // TODO
                     break;
                 case 4:
-                    System.out.println("Select"); //TODO
+                    select(cl1, cl2);
                     break;
                 case 5:
-                    remove(cl1,cl2);
+                    remove(cl1, cl2);
                     break;
                 case 6:
                     printBoth(cl1, cl2);
@@ -218,7 +210,7 @@ public class ContactList{
         }
     }
 
-    public static void intersect(ContactList cl1, ContactList cl2){
+    public static void intersect(ContactList cl1, ContactList cl2) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Group > ");
         String group = sc.nextLine();
@@ -226,49 +218,70 @@ public class ContactList{
         String attribute = sc.nextLine().toLowerCase();
         System.out.print("Enter value > ");
         String value = sc.nextLine().toLowerCase();
-        if(group.equals("1")){
-            String str = "===========================Contact List 1, Contact List 2" + 
-                         "============================";
+        if (group.equals("1")) {
+            String str = "===========================Contact List 1, Contact List 2" +
+                    "============================";
             System.out.println(str);
             System.out.println(cl1.table.intersect(attribute, value, cl2.table));
             System.out.println(str);
-        }
-        else if(group.equals("2")){
-            String str = "===========================Contact List 2, Contact List 1" + 
-                         "============================";
+        } else if (group.equals("2")) {
+            String str = "===========================Contact List 2, Contact List 1" +
+                    "============================";
             System.out.println(str);
             System.out.println(cl2.table.intersect(attribute, value, cl1.table));
             System.out.println(str);
-        }
-        else{
+        } else {
             System.out.println("Invalid group");
         }
-
     }
 
-    public static void remove(ContactList cl1, ContactList cl2){
+    public static void remove(ContactList cl1, ContactList cl2) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter attribute > ");
         String attribute = sc.nextLine().toLowerCase();
         System.out.print("Enter value > ");
         String value = sc.nextLine().toLowerCase();
-        cl1.table.remove(attribute,value);
-        cl2.table.remove(attribute,value);
-        sc.close();
-
+        cl1.table.remove(attribute, value);
+        cl2.table.remove(attribute, value);
     }
 
-    //prints out both tables with a header
-    public static void printBoth(ContactList table1, ContactList table2){
+    public static void select(ContactList cl1, ContactList cl2) {
+        Scanner selectScan = new Scanner(System.in);
+
+        System.out.print("Enter table (1/2) > ");
+        String tableNum = selectScan.nextLine().toLowerCase();
+        System.out.print("Enter attribute > ");
+        String attribute = selectScan.nextLine().toLowerCase();
+        System.out.print("Enter value > ");
+        String value = selectScan.nextLine().toLowerCase();
+        
+        if(tableNum.equals("1")){
+            String header1 = "===========================Contact List 1" +
+                "============================";
+            System.out.println(header1);
+            System.out.println(cl1.table.select(attribute, value).toString());
+            System.out.println(header1);
+        }else if(tableNum.equals("2")){
+            String header2 = "===========================Contact List 2" +
+                "============================";
+            System.out.println(header2);
+            System.out.println(cl2.table.select(attribute, value));
+            System.out.println(header2);
+        }else{
+            System.out.println("Invalid table number");
+        }
+    }
+
+    // prints out both tables with a header
+    public static void printBoth(ContactList table1, ContactList table2) {
         System.out.println("\n===========================Contact List 1============================");
         System.out.println(table1.table.toString());
         System.out.println("===========================Contact List 1============================\n");
         System.out.println("===========================Contact List 2============================");
         System.out.println(table2.table.toString());
         System.out.println("===========================Contact List 2============================\n");
-        
-    }
 
+    }
 
 }
--- Table.java
+++ Table.java
@@ -1,122 +1,136 @@
 public class Table<T extends Contact> {
-    private Node head;     // First record in the table
-    private Node tail;     // Last record in the table 
-    private String title;  // Label for the table
+    private Node head; // First record in the table
+    private Node tail; // Last record in the table
+    private String title; // Label for the table
 
-    public Table(String title){
+    public Table(String title) {
         this.title = title;
         this.head = null;
         this.tail = null;
     }
 
-    public void setHead(Node head){
+    public void setHead(Node head) {
         this.head = head;
     }
 
-    public void setTail(Node tail){
+    public void setTail(Node tail) {
         this.tail = tail;
     }
-    
+
     // Table<T> difference(Table<T> table){
-    // //Creates a new table comprised of nodes in this table, but not in table. 
-    
+    // //Creates a new table comprised of nodes in this table, but not in table.
+
     // }
 
-    //Adds a new record to the end of the this table.
-    void insert(T data){
+    // Adds a new record to the end of the this table.
+    void insert(T data) {
         Node<T> newNode = new Node(data);
-        if(head == null){ //if list is empty, set head and tail to new node
+        if (head == null) { // if list is empty, set head and tail to new node
             head = newNode;
             tail = newNode;
-        } else{ //set tail's next to new node, then update tail to the new node
+        } else { // set tail's next to new node, then update tail to the new node
             tail.setNext(newNode);
-            tail = newNode; 
-        }   
+            tail = newNode;
+        }
     }
 
-    //Creates a new table comprised of nodes having a value for a specific attribute, created from both tables.
-    Table<T> intersect(String attribute, String value, Table<T> table){
-        Table<T> result = new Table("result"); //table to return
+    // Creates a new table comprised of nodes having a value for a specific
+    // attribute, created from both tables.
+    Table<T> intersect(String attribute, String value, Table<T> table) {
+        Table<T> result = new Table("result"); // table to return
 
-        //boolean that says if the sought value has been found already
+        // boolean that says if the sought value has been found already
         boolean found = false;
 
         Node<T> current = head;
-        if(current.data.hasValue(attribute, value)){ //check the head
+        if (current.data.hasValue(attribute, value)) { // check the head
             result.insert(current.data);
             found = true;
-        } else{
-            while(current.hasNext()){ //check rest of list
+        } else {
+            while (current.hasNext()) { // check rest of list
                 current = current.next;
-                if(current.data.hasValue(attribute, value)){
+                if (current.data.hasValue(attribute, value)) {
                     result.insert(current.data);
                     found = true;
                 }
             }
         }
 
-        if(!found){ //if the sought value hasn't been found in this table, check the other table
+        if (!found) { // if the sought value hasn't been found in this table, check the other table
             current = table.head;
-            if(current.data.hasValue(attribute, value)){ //check the head
+            if (current.data.hasValue(attribute, value)) { // check the head
                 result.insert(current.data);
-            } else{
-                while(current.hasNext()){ //check rest of list
+            } else {
+                while (current.hasNext()) { // check rest of list
                     current = current.next;
-                    if(current.data.hasValue(attribute, value)){
+                    if (current.data.hasValue(attribute, value)) {
                         result.insert(current.data);
                     }
                 }
             }
-        
+
         }
-        return result; //return created table to be printed
+        return result; // return created table to be printed
     }
 
     void remove(String attribute, String value) {
-        
+
         Node<T> current = head.next;
         Node<T> previous = head;
         boolean found = false;
 
-        if(previous.data.hasValue(attribute, value)){
+        if (previous.data.hasValue(attribute, value)) {
             head = current;
             found = true;
         }
-        while(previous.hasNext() && !found){
-            
-            if(current.data.hasValue(attribute,value)){
+        while (previous.hasNext() && !found) {
+
+            if (current.data.hasValue(attribute, value)) {
                 previous.next = current.next;
                 found = true;
-            }else{
+            } else {
                 previous = current;
                 current = current.next;
             }
         }
 
     }
-    // Table<T> select(String attribute, String value){
-        
-    // //Creates a new table comprised of nodes having a value for a specific attribute. 
-    // }
+
+    Table<T> select(String attribute, String value) {
+        Table<T> result = new Table("result"); // table to return
+
+        Node<T> current = head;
+        if (current.data.hasValue(attribute, value)) { // check the head
+            result.insert(current.data);
+        }
+        while (current.hasNext()) { // check rest of list
+            current = current.next;
+            if (current.data.hasValue(attribute, value)) {
+                result.insert(current.data);
+            }
+        }
+        return result; // return created table to be printed
+    }
 
     // Table<T> union(Table<T> table){
-        
-    // //Creates a new table comprised of nodes that occur in either table(s). No duplicates allowed.
-    // }  
 
-    public String toString(){
+    // //Creates a new table comprised of nodes that occur in either table(s). No
+    // duplicates allowed.
+    // }
+
+    public String toString() {
         String string = "";
         string = head.data.toString();
 
         Node<T> current = head;
-        while(current.hasNext()){
+        while (current.hasNext()) {
             current = current.next;
             string += current.data.toString();
         }
         return string;
     }
 
-    public class Node<T>{
+    public class Node<T> {
 
         private T data;
 
@@ -127,7 +141,7 @@ public class Table<T extends Contact> {
          * 
          * @param data initializes data field
          */
-        public Node(T data){
+        public Node(T data) {
             this.data = data;
             this.next = null;
         }
@@ -135,10 +149,10 @@ public class Table<T extends Contact> {
         /**
          * Constructor that initializes fields
          * 
-         * @param data initializes data field
+         * @param data     initializes data field
          * @param nextNode initializes next field
          */
-        private Node(T data, Node<T> nextNode){
+        private Node(T data, Node<T> nextNode) {
             this.data = data;
             this.next = nextNode;
         }
@@ -148,7 +162,7 @@ public class Table<T extends Contact> {
          * 
          * @return data the data at a given node
          */
-        public T getData(){
+        public T getData() {
             return data;
         }
 
@@ -157,7 +171,7 @@ public class Table<T extends Contact> {
          * 
          * @param next to set the next field
          */
-        public void setNext(Node<T> next){
+        public void setNext(Node<T> next) {
             this.next = next;
         }
 
@@ -166,11 +180,11 @@ public class Table<T extends Contact> {
          * 
          * @param data to set the data field
          */
-        public void setData(T data){
+        public void setData(T data) {
             this.data = data;
         }
 
-        public boolean hasNext(){
+        public boolean hasNext() {
             return this.next != null;
         }
     }
